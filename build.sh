95293683bd3ffe38a545c72caae341d94e3e2dd3
dmn <dmn@starfinanz.de>
2021-08-18 20:15:23 +0200

    clang_cl test

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-conan install . --install-folder build --build=outdated -s arch_target=x86_64 -s build_type=Debug -s compiler.cppstd=17 -s compiler.version=12 -s compiler=clang -s compiler.libcxx=libstdc++11 -e CC=clang-12 -e CXX=clang++-12 
+conan install . --install-folder build --build=outdated -s arch_target=x86_64 -s build_type=Debug -s compiler.cppstd=17 -s compiler.version=16 -s compiler="Visual Studio" -s compiler.toolset=ClangCL -s compiler.runtime=MTd -e CC=clang-cl -e CXX=clang-cl
 conan build . --build-folder build
